45491df1ae3a25d17195f15bab0580cd89d76223
alex18101985 <alex18101985@gmail.com>
2026-03-16 19:25:15 +0300

    Update build.sh

--- android_kernel_xiaomi_sm8450/build.sh
+++ android_kernel_xiaomi_sm8450/build.sh
@@ -61,6 +61,7 @@ VDLKM_DIR="$KERNEL_DIR/vendor_dlkm"
 DEFCONFIG="gki_defconfig"
 DEFCONFIGS="vendor/waipio_GKI.config \
 vendor/xiaomi_GKI.config \
+vendor/susfs.config \
 vendor/westwood.config \
 vendor/debugfs.config"
 
@@ -251,12 +252,12 @@ echo_i "Generating config..."
 m $DEFCONFIG
 m ./scripts/kconfig/merge_config.sh $DEFCONFIGS vendor/${TARGET}_GKI.config
 scripts/config --file out/.config \
-    --set-str LOCALVERSION "-AOSPA-Marble-KSU" \
+    --set-str LOCALVERSION "-AOSPA-Marble-KSU-SUSFS" \
     -d LOCALVERSION_AUTO \
     -m CONFIG_KSU
 $NO_LTO && {
     scripts/config --file out/.config \
-        --set-str LOCALVERSION "-AOSPA-Marble-KSU-noLTO" \
+        --set-str LOCALVERSION "-AOSPA-Marble-KSU-SUSFS-noLTO" \
         -d LTO_CLANG_FULL -e LTO_NONE
     echo_i "Disabled LTO!"
 }
