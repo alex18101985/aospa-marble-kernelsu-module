e9e20f46ef375427ff1d80c734aaa007688999cc
alex18101985 <alex18101985@gmail.com>
2026-03-23 20:02:23 +0300

    Update build.sh

--- android_kernel_xiaomi_sm8450/build.sh
+++ android_kernel_xiaomi_sm8450/build.sh
@@ -244,11 +244,11 @@ echo_i "Generating config..."
 m $DEFCONFIG
 m ./scripts/kconfig/merge_config.sh $DEFCONFIGS vendor/${TARGET}_GKI.config
 scripts/config --file out/.config \
-    --set-str LOCALVERSION "-AOSPA-Marble-GKI-KSUNext-SUSFS" \
+    --set-str LOCALVERSION "-AOSPA-Marble-GKI-KSUNext" \
     -d LOCALVERSION_AUTO
 $NO_LTO && {
     scripts/config --file out/.config \
-        --set-str LOCALVERSION "-AOSPA-Marble-GKI-KSUNext-SUSFS-noLTO" \
+        --set-str LOCALVERSION "-AOSPA-Marble-GKI-KSUNext-noLTO" \
         -d LTO_CLANG_FULL -e LTO_NONE
     echo_i "Disabled LTO!"
 }
